a30fa379797e4cb5dba467aa6ef26ced978708ac
bitsianrk <30334342+bitsianrk@users.noreply.github.com>
2022-02-25 13:57:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-echo "# streamlit-to-heroku-tutorial" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/your_username/your_repo_name.git
-git push -u origin master
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
